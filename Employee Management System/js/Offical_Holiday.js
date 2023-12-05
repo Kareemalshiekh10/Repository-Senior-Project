6127832c5f4884c70c6b4436761d217f3e3f9cd8
Ahmed-Nasser612 <ahmed.nasser.hamdy@gmail.com>
2023-12-06 01:59:33 +0200

    official&annual(add-edit)Pages

--- Employee Management System/js/Offical_Holiday.js
+++ Employee Management System/js/Offical_Holiday.js
@@ -1,44 +1,24 @@
-function deleteHoliday(HolidayId) {
-    // Implement your deletion logic here
-    // For now, let's just remove the row from the table
-    const row = document.querySelector(`tr[data-id="${HolidayId}"]`);
-    if (row) {
-        row.remove();
+function deleteEmployee(employeeId) {
+    const confirmed = confirm("Are you sure you want to delete this Holiday?");
+    if (confirmed) {
+        // Implement your deletion logic here
+        // For now, let's just remove the row from the table
+        const row = document.querySelector(`tr[data-id="${employeeId}"]`);
+        if (row) {
+            row.remove();
+        }
+    } else {
+        // User clicked Cancel, do nothing or handle accordingly
+        console.log("Deletion canceled.");
     }
 }
 
-function editHoliday(id) {
-    const row = document.querySelector(`tr[data-id="${id}"]`);
-    const cells = row.querySelectorAll('td');
+function redirectToEditPage(employeeId) {
+    // Assuming the page is in the same directory, adjust the path accordingly
+    window.location.href = "Edit_Official_Holidays.html";
+  }
+  
 
-    cells.forEach(cell => {
-        const currentValue = cell.textContent.trim();
-        cell.innerHTML = `<input type="text" value="${currentValue}" />`;
-    });
 
-    const actionCell = row.querySelector('td:last-child');
-    actionCell.innerHTML = `<button onclick="saveHoliday(${id})">Save</button>
-                            <button onclick="deleteHoliday(${id})">Delete</button>`;
-}
-
-function saveHoliday(id) {
-    const row = document.querySelector(`tr[data-id="${id}"]`);
-    const cells = row.querySelectorAll('td:not(:last-child)'); // Select all td except the last one
-
-    cells.forEach(cell => {
-        const newValue = cell.querySelector('input').value;
-        cell.textContent = newValue;
-    });
-
-    const actionCell = row.querySelector('td:last-child');
-    actionCell.innerHTML = `<button onclick="deleteHoliday(${id})">Delete</button>
-                            <button onclick="editHoliday(${id})">Edit</button>`;
-}
-
-// Function to save the edited information after re-editing
-function saveEditedHoliday(id) {
-    const row = document.querySelector(`tr[data-id="${id}"]`);
-    editHoliday(id); // Re-enable editing mode by calling the editEmployee function
-}
 
 
